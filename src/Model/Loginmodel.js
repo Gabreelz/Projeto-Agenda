85aadf1070ad96f04b13a2b103c43c25607d2699
Gabreelz <gabriel_teixeira30@hotmail.com>
2026-02-23 19:58:32 -0300

    Finalizando as config do Login

--- src/Model/Loginmodel.js
+++ src/Model/Loginmodel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -18,13 +19,23 @@ class Login {
     async register(){
         this.valida();
         if(this.errors.length > 0) return;
+       
+        this.userExists();
         
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
             this.user = await Loginmodel.create(this.body);
         } catch(e) {
             console.log(e);
         }
     }
+
+      async userExists() {
+        const user = await LoginModel.findOne({ email: this.body.email });
+        if(user) this.errors.push('Usuário já existe.');
+  }
 
         valida() {
         this.cleanUp();
