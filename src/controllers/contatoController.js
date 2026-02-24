2977bc355dfa32aa6cdad4f31bf54199ff2d46e7
Gabreelz <gabriel_teixeira30@hotmail.com>
2026-02-24 16:53:30 -0300

    Teste

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,28 @@
+const ContatoModel = require('../Model/ContatoModel');
+
+exports.index = (req, res) => {
+  res.render('contato', {
+    contato: {}
+  });
+};
+
+exports.register = async (req, res) => {
+    try{
+        const contato = new ContatoModel(req.body);
+
+            await contato.register();
+
+            if(contato.errors.length > 0) {
+                req.flash('errors', contato.errors);
+                req.session.save(() => res.redirect('/contato/index'));
+                return;
+            }
+
+            req.flash('success', 'Contato registrado com sucesso.');
+            req.session.save(() => res.redirect('/'));
+            return;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+}
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -21,3 +21,13 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login para acessar essa página.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
+  next();
+}
