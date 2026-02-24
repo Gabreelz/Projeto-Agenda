7d93a83d46551c3bf606622026d248fee637b81e
Gabreelz <gabriel_teixeira30@hotmail.com>
2026-02-24 17:03:59 -0300

    Cadastrando contatos (Create)

--- src/Model/ContatoModel.js
+++ src/Model/ContatoModel.js
@@ -3,58 +3,58 @@ const validator = require('validator');
 
 const ContatoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
-  sobrenome: { type: String, default: '' },
-  email: { type: String, default: '' },
-  telefone: { type: String, default: '' },
-  criadoEm: { type: Date, default: Date.now }
+  sobrenome: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  telefone: { type: String, required: false, default: '' },
+  criadoEm: { type: Date, default: Date.now },
 });
 
 const ContatoModel = mongoose.model('Contato', ContatoSchema);
 
-class Contato {
-  constructor(body) {
-    this.body = body;
-    this.errors = [];
-    this.contato = null;
-  }
-
-  async register() {
-    this.valida();
-    if (this.errors.length > 0) return;
-
-    this.contato = await ContatoModel.create(this.body);
-  }
-
-  valida() {
-    this.cleanUp();
-
-    if (!this.body.nome) {
-      this.errors.push('O nome é obrigatório.');
-    }
-
-    if (this.body.email && !validator.isEmail(this.body.email)) {
-      this.errors.push('E-mail inválido.');
-    }
-
-    if (!this.body.email && !this.body.telefone) {
-      this.errors.push('Informe pelo menos email ou telefone.');
-    }
-  }
-
-  cleanUp() {
-    for (const key in this.body) {
-      if (typeof this.body[key] !== 'string') {
-        this.body[key] = '';
-      }
-    }
-
-    this.body = {
-      nome: this.body.nome,
-      sobrenome: this.body.sobrenome,
-      email: this.body.email,
-      telefone: this.body.telefone
-    };
-  }
+function Contato(body) {
+  this.body = body;
+  this.errors = [];
+  this.contato = null;
 }
+
+Contato.buscaPorId = async function(id) {
+  if(typeof id !== 'string') return;
+  const user = await ContatoModel.findById(id);
+  return user;
+};
+
+Contato.prototype.register = async function() {
+  this.valida();
+  if(this.errors.length > 0) return;
+  this.contato = await ContatoModel.create(this.body);
+};
+
+Contato.prototype.valida = function() {
+  this.cleanUp();
+
+  // Validação
+  // O e-mail precisa ser válido
+  if(this.body.email && !validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+  if(!this.body.nome) this.errors.push('Nome é um campo obrigatório.');
+  if(!this.body.email && !this.body.telefone) {
+    this.errors.push('Pelo menos um contato precisa ser enviado: e-mail ou telefone.');
+  }
+};
+
+Contato.prototype.cleanUp = function() {
+  for(const key in this.body) {
+    if(typeof this.body[key] !== 'string') {
+      this.body[key] = '';
+    }
+  }
+
+  this.body = {
+    nome: this.body.nome,
+    sobrenome: this.body.sobrenome,
+    email: this.body.email,
+    telefone: this.body.telefone,
+  };
+};
+
 
 module.exports = Contato;
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -6,23 +6,31 @@ exports.index = (req, res) => {
   });
 };
 
-exports.register = async (req, res) => {
-    try{
-        const contato = new ContatoModel(req.body);
+exports.register = async(req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
 
-            await contato.register();
-
-            if(contato.errors.length > 0) {
-                req.flash('errors', contato.errors);
-                req.session.save(() => res.redirect('/contato/index'));
-                return;
-            }
-
-            req.flash('success', 'Contato registrado com sucesso.');
-            req.session.save(() => res.redirect('/'));
-            return;
-    } catch(e) {
-        console.log(e);
-        return res.render('404');
+    if(contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('back'));
+      return;
     }
-}
+
+    req.flash('success', 'Contato registrado com sucesso.');
+    req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+    return;
+  } catch(e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.editIndex = async function(req, res) {
+  if(!req.params.id) return res.render('404');
+
+  const contato = await Contato.buscaPorId(req.params.id);
+  if(!contato) return res.render('404');
+
+  res.render('contato', { contato });
+};
